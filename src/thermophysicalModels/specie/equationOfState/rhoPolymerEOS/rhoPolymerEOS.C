5cec1a3c5028603065dc9bc30f153dacfe75266d
Anuar R. Giménez <anuargimenez@usal.es>
2022-05-02 19:50:17 +0200

    Tait EOS Eq. and Dictionary input reading

--- src/thermophysicalModels/specie/equationOfState/rhoPolymerEOS/rhoPolymerEOS.C
+++ src/thermophysicalModels/specie/equationOfState/rhoPolymerEOS/rhoPolymerEOS.C
@@ -27,11 +27,40 @@ License
 #include "IOstreams.H"
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+template<class Specie>
+Foam::rhoPolymerEOS<Specie>::rhoPolymerEOS(Istream& is)
+:
+    Specie(is),
+    b1m_(readScalar(is)),
+    b2m_(readScalar(is)),
+    b3m_(readScalar(is)),
+    b4m_(readScalar(is)),
+    b1s_(readScalar(is)),
+    b2s_(readScalar(is)),
+    b3s_(readScalar(is)),
+    b4s_(readScalar(is)),
+    b5_(readScalar(is)),
+    b6_(readScalar(is))
+{
+    is.check("rhoPolymerEOS<Specie>::rhoPolymerEOS(Istream& is)");
+}
+
 
 template<class Specie>
 Foam::rhoPolymerEOS<Specie>::rhoPolymerEOS(const dictionary& dict)
 :
-    Specie(dict)
+    Specie(dict),
+    b1m_(readScalar(dict.subDict("equationOfState").lookup("b1m"))),
+    b2m_(readScalar(dict.subDict("equationOfState").lookup("b2m"))),
+    b3m_(readScalar(dict.subDict("equationOfState").lookup("b3m"))),
+    b4m_(readScalar(dict.subDict("equationOfState").lookup("b4m"))),
+    b1s_(readScalar(dict.subDict("equationOfState").lookup("b1s"))),
+    b2s_(readScalar(dict.subDict("equationOfState").lookup("b2s"))),
+    b3s_(readScalar(dict.subDict("equationOfState").lookup("b3s"))),
+    b4s_(readScalar(dict.subDict("equationOfState").lookup("b4s"))),
+    b5_(readScalar(dict.subDict("equationOfState").lookup("b5"))),
+    b6_(readScalar(dict.subDict("equationOfState").lookup("b6")))
+
 {}
 
 
@@ -41,15 +70,43 @@ template<class Specie>
 void Foam::rhoPolymerEOS<Specie>::write(Ostream& os) const
 {
     Specie::write(os);
-}
 
+    dictionary dict("equationOfState");
+    dict.add("b1m", b1m_);
+    dict.add("b2m", b2m_);
+    dict.add("b3m", b3m_);
+    dict.add("b4m", b4m_);
+    dict.add("b1s", b1s_);
+    dict.add("b2s", b2s_);
+    dict.add("b3s", b3s_);
+    dict.add("b4s", b4s_);
+    dict.add("b5", b5_);
+    dict.add("b6", b6_);
+
+
+
+    os  << indent << dict.dictName() << dict;
+}
 
 // * * * * * * * * * * * * * * * Ostream Operator  * * * * * * * * * * * * * //
 
 template<class Specie>
 Foam::Ostream& Foam::operator<<(Ostream& os, const rhoPolymerEOS<Specie>& pg)
 {
-    pg.write(os);
+    os  << static_cast<const Specie&>(pg)
+        << token::SPACE << pf.b1m_
+        << token::SPACE << pf.b2m_
+        << token::SPACE << pf.b3m_
+        << token::SPACE << pf.b4m_
+        << token::SPACE << pf.b1s_
+        << token::SPACE << pf.b2s_
+        << token::SPACE << pf.b3s_
+        << token::SPACE << pf.b4s_
+        << token::SPACE << pf.b5_
+        << token::SPACE << pf.b6_;
+
+
+    os.check("Ostream& operator<<(Ostream&, const rhoPolymerEOS<Specie>&)");
     return os;
 }
 
